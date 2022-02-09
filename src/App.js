2165d34318ce331d779aaf9e2ed242f1551df06d
seriokim <atomyoun@gmail.com>
2022-02-09 22:52:57 +0900

    React Class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬 독학']);
+  let [따봉, 따봉변경] = useState(0);
+
+  function 제목바꾸기() {
+    const newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경( newArray );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+        <div className="black-nav">
+          <div>개발 Blog</div>
+        </div>
+        <div className='list'>
+          <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
+          <p>2월 17일 발행</p>
+          <hr/>
+        </div>
+        <div className='list'>
+          <h3> { 글제목[1] } </h3>
+          <p>2월 18일 발행</p>
+          <hr/>
+        </div>
+        <div className='list'>
+          <h3> { 글제목[2] } </h3>
+          <p>2월 19일 발행</p>
+          <hr/>
+        </div>
+
+        <Modal />
+        
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날씨</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
